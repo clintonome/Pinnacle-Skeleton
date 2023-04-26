370b9529230f0a4f6f82a7bc4536234d4287797b
Clinton Ometie <P2694809@my365.dmu.ac.uk>
2023-04-26 18:48:32 +0100

    Week 25

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+
+    {
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        clsCustomer mThisCustomer = new clsCustomer();
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                return mCustomerList;
+            }
+            set
+            {
+                mCustomerList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mCustomerList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsCustomer ThisCustomer
+        {
+            get
+            {
+                return mThisCustomer;
+            }
+            set
+            {
+                mThisCustomer = value;
+            }
+        }
+
+        public clsCustomerCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblCustomer_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsCustomer aCustomer = new clsCustomer();
+                aCustomer.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["EmailAlerts"]);
+                aCustomer.CusAdress = Convert.ToString(DB.DataTable.Rows[Index]["CusAdress"]);
+                aCustomer.CusEmail = Convert.ToString(DB.DataTable.Rows[Index]["CusEmail"]);
+                aCustomer.CusPassword = Convert.ToString(DB.DataTable.Rows[Index]["CusPassword"]);
+                aCustomer.CusUsername = Convert.ToString(DB.DataTable.Rows[Index]["CusUsername"]);
+                aCustomer.CusId = Convert.ToInt32(DB.DataTable.Rows[Index]["CusId"]);
+                mCustomerList.Add(aCustomer);
+                Index++;
+            }
+        }
+    }
+
+}
---
+++ Testing1/tstCustomerCollections.cs
@@ -0,0 +1,110 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+
+
+
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsCustomerCollection allCustomer = new clsCustomerCollection();
+            Assert.IsNotNull(allCustomer);
+        }
+
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            //Creates instance
+            clsCustomerCollection allCustomer = new clsCustomerCollection();
+            //Creates some test data to assign to the property
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            //Add an item to the list
+            //Create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.Active = true;
+            TestItem.CusId = 1;
+            TestItem.CusUsername = "benjames";
+            TestItem.CusPassword = "password1";
+            TestItem.CusAdress = "le2 7ea";
+            TestItem.CusEmail = "benjames@gmail";
+            TestItem.CusDOB = DateTime.Now.Date;
+            //Add the item to the test list
+            TestList.Add(TestItem);
+            //assign data to the property
+            allCustomer.CustomerList = TestList;
+            //test to see if the two value are the same
+            Assert.AreEqual(allCustomer.CustomerList, TestList);
+        }
+        [TestMethod]
+        public void CountPropertyOk()
+        {
+            clsCustomerCollection allCustomer = new clsCustomerCollection();
+            Int32 SomeCount = 0;
+            allCustomer.Count = SomeCount;
+            Assert.AreEqual(allCustomer.Count, SomeCount);
+        }
+        [TestMethod]
+        public void ThisCustomerPropertyOk()
+        {
+            clsCustomerCollection allCustomer = new clsCustomerCollection();
+            clsCustomer TestCustomer = new clsCustomer();
+            TestCustomer.Active = true;
+            TestCustomer.CusId = 1;
+            TestCustomer.CusUsername = "benjames";
+            TestCustomer.CusPassword = "password1";
+            TestCustomer.CusAdress = "le2 7ea";
+            TestCustomer.CusEmail = "benjames@gmail";
+            TestCustomer.CusDOB = DateTime.Now.Date;
+            allCustomer.ThisCustomer = TestCustomer;
+        }
+
+        [TestMethod]
+        public void CustomerListOk()
+        {
+            //Creates instance
+            clsCustomerCollection allCustomer = new clsCustomerCollection();
+            //Creates some test data to assign to the property
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            //Add an item to the list
+            //Create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.Active = true;
+            TestItem.CusId = 1;
+            TestItem.CusUsername = "benjames";
+            TestItem.CusPassword = "password1";
+            TestItem.CusAdress = "le2 7ea";
+            TestItem.CusEmail = "benjames@gmail";
+            TestItem.CusDOB = DateTime.Now.Date;
+            TestList.Add(TestItem);
+            allCustomer.CustomerList = TestList;
+
+        }
+
+        [TestMethod]
+        public void CustomerPropertyOk()
+        {
+            clsCustomerCollection allCustomer = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.Active = true;
+            TestItem.CusUsername = "BradJohn";
+            TestItem.CusId = 1;
+            TestItem.CusPassword = "Passsword 1";
+            TestItem.CusEmail = "BradJohn@gmail.com";
+            TestItem.CusAdress = "LE2 1EE";
+            TestItem.CusDOB = DateTime.Now.Date;
+            //allCustomer.(TestItem);
+
+        }
+
+
+
+    }
+}
